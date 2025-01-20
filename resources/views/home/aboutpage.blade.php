6597cdd793cecc7867a78873a307210fe7fed8b7
SakibMizan <sakib2154901009@student.bup.edu.bd>
2025-01-21 05:49:15 +0600

    reduced code in aboutpage, linked homecss & footer

--- resources/views/home/aboutpage.blade.php
+++ resources/views/home/aboutpage.blade.php
@@ -2,37 +2,7 @@
 <html lang="en">
 
 <head>
-    <!-- basic -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- mobile metas -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-    <!-- site metas -->
-    <title>About</title>
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!-- bootstrap css -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <!-- style css -->
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <!-- Responsive-->
-    <link rel="stylesheet" href="css/responsive.css">
-    <!-- fevicon -->
-    <link rel="icon" href="images/fevicon.png" type="image/gif" />
-    <!-- Scrollbar Custom CSS -->
-    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
-    <!-- Tweaks for older IEs-->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-    <!-- fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700|Righteous&display=swap" rel="stylesheet">
-    <!-- owl stylesheets -->
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-
+    @include('home.homecss')
     <style>
         .about_img {
             width: 600px;
@@ -231,33 +201,10 @@
         </div>
     </div>
     <!--experience section end-->
-    <!-- footer section start -->
-    <div class="footer_section layout_padding">
-        <div class="container">
-            <div class="location_main">
-                <div class="call_text"><img src="images/call-icon.png"></div>
-                <div class="call_text"><a href="tel:{{$aboutme->phone}}">{{$aboutme->phone}}</a></div>
-                <div class="call_text"><img src="images/mail-icon.png"></div>
-                <div class="call_text"><a href="mailto:demo@gmail.com">{{$aboutme->email}}</a></div>
-            </div>
-            <div class="social_icon">
-                <ul>
-                    <li><a href="https://www.facebook.com" target="blank"><img src="images/fb-icon.png"></a></li>
-                    <li><a href="https://www.x.com" target="blank"><img src="images/twitter-icon.png"></a></li>
-                    <li><a href="https://www.linkedin.com/in/sakib-mizan-r44/" target="_blank"><img src="images/linkedin-icon.png"></a></li>
-                    <li><a href="https://www.instagram.com" target="_blank"><img src="images/instagram-icon.png"></a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <!-- footer section end -->
-    <!-- copyright section start -->
-    <div class="copyright_section">
-        <div class="container">
-            <p class="copyright_text">2020 All Rights Reserved. Design by <a href="https://html.design">Sakib Mizan</a></p>
-        </div>
-    </div>
-    <!-- copyright section end -->
+    <!-- footer & copyright section start -->
+    @include('home.footer')
+    <!-- footer & copyright section end -->
+
     <!-- Javascript files-->
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.min.js"></script>
