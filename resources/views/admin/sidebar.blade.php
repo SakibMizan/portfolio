49519e2172eae77d8f5bf24093d699d672387cba
SakibMizan <sakib2154901009@student.bup.edu.bd>
2025-01-21 07:04:28 +0600

    sidebar & navbar updated. image updated

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,10 +1,10 @@
 <nav id="sidebar">
     <!-- Sidebar Header-->
     <div class="sidebar-header d-flex align-items-center">
-        <div class="avatar"><img src="adminpanel/img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
+        <div class="avatar"><img src="adminpanel/img/2023.1-modified.png" alt="..." class="img-fluid rounded-circle"></div>
         <div class="title">
-            <h1 class="h5">Mark Stephen</h1>
-            <p>Web Designer</p>
+            <h1 class="h5">Sakib Mizan</h1>
+            <p>Web Developer</p>
         </div>
     </div>
     <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
