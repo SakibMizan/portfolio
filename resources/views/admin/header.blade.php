245a15c2847a69472f5b845095b4a5dde241e3b3
SakibMizan <sakib2154901009@student.bup.edu.bd>
2025-01-21 07:24:02 +0600

    added welcome msg & image to admin panel home page

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -5,7 +5,7 @@
             <div class="navbar-header">
                 <!-- Navbar Header--><a href="index.html" class="navbar-brand">
                     <div class="brand-text brand-big visible text-uppercase"><strong class="text-primary">Admin</strong><strong>Panel</strong></div>
-                    <div class="brand-text brand-sm"><strong class="text-primary">D</strong><strong>A</strong></div>
+                    <div class="brand-text brand-sm"><strong class="text-primary">A</strong><strong>P</strong></div>
                 </a>
                 <!-- Sidebar Toggle Btn-->
                 <button class="sidebar-toggle"><i class="fa fa-long-arrow-left"></i></button>
